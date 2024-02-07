51d45a5014d561b3599abce43bf1c85d5fafafec
Irina <dykinairina@gmail.com>
2024-02-07 22:49:49 +0100

    Zadanie 2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Элементы массива, начиная с конца:");
+        PrintArrayReversed(array, array.Length - 1);
+    
+
+    static void PrintArrayReversed(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write(arr[index] + " ");
+            PrintArrayReversed(arr, index - 1);
+        }
+    }
